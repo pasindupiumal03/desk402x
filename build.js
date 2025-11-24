9c696582a4890c0771dd5ceb1990c9832779bb55
Pasindu <163386074+pasindupiumal03@users.noreply.github.com>
2025-11-24 10:15:19 +0530

    Final static site configuration for Vercel deployment

---
+++ build.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// Simple build script for static site deployment
+console.log('Building static site...');
+console.log('Static site is ready - no build process needed');
+process.exit(0);
